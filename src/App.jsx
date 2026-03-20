ab44d5b2ff5245cb12fadb6937fd6afbe9600fbe
sofiacera <sofiacera01gmail.com>
2026-03-21 02:49:37 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/about' element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return <div className="min-h-screen ">
+
+
+
+
+
+
+  </div>;
+
+};
